7065cff2d0b0ae2b0e320d42daa0af85a6f3ad9e
jingshouyan <jingshouyan@gmail.com>
2026-07-22 21:28:12 +0800

    init

---
+++ src/main/java/com/github/jingshouyan/rag4j101/Rag4j101Application.java
@@ -0,0 +1,13 @@
+package com.github.jingshouyan.rag4j101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rag4j101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rag4j101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jingshouyan/rag4j101/Rag4j101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jingshouyan.rag4j101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rag4j101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
